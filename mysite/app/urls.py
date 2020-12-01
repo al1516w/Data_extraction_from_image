5f4c355c9b7c4315346e3ef9d46a3728532f8f34
al1516w <bharathsingh11.bs@gmail.com>
2020-12-01 16:58:09 +0530

    made the flow better

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload', views.uploadimage, name='uploadimage')
+    path('upload', views.uploadimage, name='uploadimage'),
+    path('view_uploads', views.view_uploads, name='view_uploads'),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -7,18 +7,21 @@ from PIL import Image
 
 
 def index(request):
-    objects = Imagee.objects.all()
-    lt = []
-    for obj in objects:
-        print(obj.pic.path)
-        im1 = Image.open(obj.pic.path)
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\HP\AppData\Local\Tesseract-OCR\tesseract.exe'
-        lt.append((pytesseract.image_to_string(im1, lang='eng', config='--psm ' + str(6)), obj))
-    return render(request, 'app/index.html', {'objects': lt})
+    return render(request, 'app/index.html')
 
 
 def uploadimage(request):
     p = request.FILES['image']
     obj = Imagee(pic=p)
     obj.save()
-    return render(request, 'app/index.html')
+    return render(request, 'app/upload.html')
+
+
+def view_uploads(request):
+    objects = Imagee.objects.all()
+    lt = []
+    for obj in objects:
+        im1 = Image.open(obj.pic.path)
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Users\HP\AppData\Local\Tesseract-OCR\tesseract.exe'
+        lt.append((pytesseract.image_to_string(im1, lang='eng', config='--psm ' + str(6)), obj))
+    return render(request, 'app/view_uploads.html', {'objects': lt})
